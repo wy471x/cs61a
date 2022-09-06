d9bc9ac6dc0f8906d5d8099f1ebfd5aadcf51cbe
wy471x <wy471x@gmail.com>
2022-09-06 22:30:17 +0800

    cs61a su2019 hw06 finishing.

--- su2019/hws/hw06/hw06.scm
+++ su2019/hws/hw06/hw06.scm
@@ -15,7 +15,7 @@
   (car (cdr (cdr s)))
 )
 
-(define (unique s)
+(define (unique s)  
   (cond
    ((null? s) s)
    (else (cons (car s) (unique (filter (lambda (x) (not (eq? x (car s)))) (cdr s)))))
@@ -29,20 +29,29 @@
 ;; List all ways to make change for TOTAL with DENOMS
 (define (list-change total denoms)
   'YOUR-CODE-HERE
-  )
+)
 
 ; Tail recursion
 
 (define (replicate x n)
-  'YOUR-CODE-HERE
+  (define (replicate-tail x n result)
+    (if (= n 0) result
+      (replicate-tail x (- n 1) (append result (list x)))
+    )
   )
+  (replicate-tail x n (list))
+)
 
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE
+  (if (= n 1) (combiner start (term n)) 
+    (combiner (term n) (accumulate combiner start (- n 1) term))
+  )
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (if (= n 0) start
+    (accumulate-tail combiner (combiner start (term n)) (- n 1) term)
+  )
 )
 
 
